ea235e12f993b1c29ffe64b0ff1d8ee4bf3922e8
wrong89 <fenomen_03@mail.ru>
2025-10-07 20:53:00 +0300

    added slider

--- src/components/ImageSlider/ImageSlider.tsx
+++ src/components/ImageSlider/ImageSlider.tsx
@@ -1,43 +1,84 @@
-import Slider from "react-slick";
+// import Slider from "react-slick";
 
+// import "slick-carousel/slick/slick-theme.css";
+// import "slick-carousel/slick/slick.css";
+
+
+// const ImageSlider = ({ }) => {
+//     var settings = {
+//         infinite: true,
+//         speed: 500,
+//         slidesToShow: 3,
+//         slidesToScroll: 1
+//     };
+//     return (
+//         <Slider {...settings}>
+//             <div>
+//                 <img src="../../../public/assets/1.jpg" alt="" />
+//             </div>
+//             <div>
+//                 <img src="../../../public/assets/2.jpg" alt="" />
+//             </div>
+//             <div>
+//                 <img src="../../../public/assets/3.jpg" alt="" />
+//             </div>
+//             <div>
+//                 <h3>4</h3>
+//             </div>
+//             <div>
+//                 <h3>5</h3>
+//             </div>
+//             <div>
+//                 <h3>6</h3>
+//             </div>
+//         </Slider>
+//     );
+// };
+
+// export default ImageSlider;
+
+
+import Slider from "react-slick";
 import "slick-carousel/slick/slick-theme.css";
 import "slick-carousel/slick/slick.css";
 
-// interface ImageSliderProps {
-//     images: string[];
-//     width?: number | string;
-//     height?: number | string;
-//     interval?: number; // интервал в миллисекундах
-// }
+import cls from "./ImageSlider.module.scss";
 
-const ImageSlider = ({ }) => {
-    var settings = {
+const ImageSlider = () => {
+    const settings = {
         infinite: true,
         speed: 500,
         slidesToShow: 3,
-        slidesToScroll: 1
+        slidesToScroll: 2,
+        autoplay: true,         // автоматическое перелистывание
+        autoplaySpeed: 3000,    // каждые 3 секунды
+        pauseOnHover: true,     // можно остановить при наведении
+        arrows: false,          // отключим стрелки, если не нужны
     };
+
     return (
-        <Slider {...settings}>
-            <div>
-                <h3>1</h3>
-            </div>
-            <div>
-                <h3>2</h3>
-            </div>
-            <div>
-                <h3>3</h3>
-            </div>
-            <div>
-                <h3>4</h3>
-            </div>
-            <div>
-                <h3>5</h3>
-            </div>
-            <div>
-                <h3>6</h3>
-            </div>
-        </Slider>
+        <div className={cls.slider}>
+            <Slider {...settings}>
+                <div className={cls.slider__slide}>
+                    <h3 className={cls.slider__content}>1</h3>
+                </div>
+                <div className={cls.slider__slide}>
+                    <h3 className={cls.slider__content}>2</h3>
+                </div>
+                <div className={cls.slider__slide}>
+                    <h3 className={cls.slider__content}>3</h3>
+                </div>
+                <div className={cls.slider__slide}>
+                    <h3 className={cls.slider__content}>4</h3>
+                </div>
+                <div className={cls.slider__slide}>
+                    <h3 className={cls.slider__content}>5</h3>
+                </div>
+                <div className={cls.slider__slide}>
+                    <h3 className={cls.slider__content}>6</h3>
+                </div>
+            </Slider>
+        </div>
     );
 };
 
--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -3,8 +3,7 @@ import ImageSlider from "@/components/ImageSlider/ImageSlider"
 const MainPage = () => {
     return (
         <>
-            <h1>Hello From Main page</h1>
-            <ImageSlider  />
+            <ImageSlider />
         </>
     )
 }
